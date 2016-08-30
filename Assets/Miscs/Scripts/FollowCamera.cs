8adcb40080bd41a9156e92fd35f078e72e80eb62
Redouane <o-red@outlook.com>
2016-08-31 00:58:52 +0100

    Initial commit

---
+++ Assets/Miscs/Scripts/FollowCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+    public float damping = 4f;
+    public Transform target;
+    public float distance = 4f;
+    public float height = 1.5f;
+
+    private Vector3 cameraPosition;
+    private Vector3 newFollowPosition;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!target)
+            return;
+
+        cameraPosition = new Vector3(0, -height, distance);
+        newFollowPosition = target.position - cameraPosition;
+        transform.position = Vector3.Slerp(transform.position, newFollowPosition, damping);
+	}
+}
